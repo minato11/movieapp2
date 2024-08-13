b36e9150afdbb1cd5aff42e61ddb66a924c7c37c
Kolya Shevchenko <98040306+minato11@users.noreply.github.com>
2024-08-13 17:16:06 +0300

    final review fix

--- components/Header.tsx
+++ components/Header.tsx
@@ -67,7 +67,7 @@ const Header = () => {
 
                         <PopoverPanel
                             transition
-                            className="absolute -left-8 top-full z-10 mt-3 w-screen max-w-md overflow-hidden rounded-3xl bg-white shadow-lg ring-1 ring-gray-900/5 transition data-[closed]:translate-y-1 data-[closed]:opacity-0 data-[enter]:duration-200 data-[leave]:duration-150 data-[enter]:ease-out data-[leave]:ease-in "
+                            className="absolute -left-8 top-full z-10 mt-3 w-screen max-w-md overflow-hidden rounded-3xl bg-white shadow-lg ring-1 ring-gray-900/5 transition data-[closed]:translate-y-1 data-[closed]:opacity-0 data-[enter]:duration-200 data-[leave]:duration-150 data-[enter]:ease-out data-[leave]:ease-in  "
                         >
                             <div className="p-1">
                                 {genres.map((genres) => (
@@ -104,7 +104,7 @@ const Header = () => {
                     </a>
                 </div>
             </nav>
-            <Dialog open={mobileMenuOpen} onClose={setMobileMenuOpen} className="lg:hidden ">
+            <Dialog open={mobileMenuOpen} onClose={setMobileMenuOpen} className="lg:hidden  ">
                 <div className="fixed inset-0 z-10 "/>
                 <DialogPanel
                     className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10 dark:bg-dark">
@@ -112,7 +112,7 @@ const Header = () => {
                         <a href="/" className="-m-1.5 p-1.5">
                             <span className="sr-only"></span>
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5}
-                                 stroke="currentColor" className="size-6">
+                                 stroke="currentColor" className="h-6 w-6 dark:text-white">
                                 <path strokeLinecap="round" strokeLinejoin="round"
                                       d="m2.25 12 8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25"/>
                             </svg>
@@ -120,7 +120,7 @@ const Header = () => {
                         <button
                             type="button"
                             onClick={() => setMobileMenuOpen(false)}
-                            className="-m-2.5 rounded-md p-2.5 text-gray-700"
+                            className="-m-2.5 rounded-md p-2.5 text-gray-700 dark:text-white"
                         >
                             <span className="sr-only ">Close menu</span>
                             <XMarkIcon aria-hidden="true" className="h-6 w-6"/>
